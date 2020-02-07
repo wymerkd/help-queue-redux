8e2359f1be73f48c64489f2f6ddc746389bfbaa3
Dylan Manley <dylan.manley@gmail.com>
2020-02-07 15:03:33 -0800

    install bable, and config app for bable

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
+  // ReactDOM.render(
+  //   <div>
+  //   <h1>Help Queue T</h1>
+  //   <h3>3a</h3>
+  //   <h3>Thato and Haley</h3>
+  //   <h5><em>Firebase will not save to record</em></h5>
+  //   </div>,
+  //   document.getElementById('react-app-root')
+  // );
