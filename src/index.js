b331ba6f9ee42d877e54fd339f2251a6e972c7af
Dylan Manley <dylan.manley@gmail.com>
2020-02-07 14:25:35 -0800

    render without jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-function liveTime() {
-  var greeting = React.createElement('h1', {}, 'Hello, World!');
-  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-  var app = React.createElement('div', {}, greeting, clock);
+  var headding = React.createElement('h1', {}, 'Help Queue T');
+  var ticketLocation = React.crateElements('h3', {}, '3a');
+  var ticketNames = React.createElement('h3', {}, 'Thato and Haley')
+  var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
+  var app = React.createElement('div', {}, heading, ticketLocation, ticketTimes, ticketIssues);
 
   ReactDOM.render(
     app,
@@ -9,5 +10,6 @@ function liveTime() {
   );
 }
 
-
-setInterval(liveTime, 1000);
+// function liveTime() {
+  // var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+// setInterval(liveTime, 1000);
