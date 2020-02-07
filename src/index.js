15eebfabad878e99173882a69161904165f94f19
Dylan Manley <dylan.manley@gmail.com>
2020-02-07 14:00:12 -0800

    make clock adjust every seccond

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-var greeting = React.createElement('h1', {}, 'Hello, World!');
+function liveTime() {
+  var greeting = React.createElement('h1', {}, 'Hello, World!');
+  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  var app = React.createElement('div', {}, greeting, clock);
 
-ReactDOM.render(
-  greeting,
-  document.getElementById('react-app-root')
-);
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+}
+
+
+setInterval(liveTime, 1000);
