f4819763e90798699798e35fd2d7a8d6fb66997a
Dylan Manley <dylan.manley@gmail.com>
2020-02-07 16:07:53 -0800

    create embeded js file, dlete index.html

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket";
+import PropTypes from "prop-types";
+
+function TicketList(){
+  return (
+    <Ticket
+    location ='3A'
+    names='thato and haley'
+    issue='firebase will not record'/>
+  );
+}
+
+export default TicketList;
