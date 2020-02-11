764d1698690ecfcda53cf5fd3e3478933838b05c
Dylan Manley <dylan.manley@gmail.com>
2020-02-10 22:22:56 -0800

    checge ticket.jsx to work with css better

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function Ticket(props){
   return (
     <div>
-        <style jsx>{`
+        <style global jsx>{`
           div {
             background-color: red;
           }
