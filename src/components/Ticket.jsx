f5cddaf7ef0b0c3ddd4aa2e238e1146c834ece27
Dylan Manley <dylan.manley@gmail.com>
2020-02-10 22:14:18 -0800

    add css styling

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Ticket(){
+function Ticket(props){
   return (
     <div>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
+        <style jsx>{`
+          div {
+            background-color: red;
+          }
+        `}</style>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
   );
 }
 
