e8c2df498fa8a06ffffcf0b7e91745ba7fb5ce43
Dylan Manley <dylan.manley@gmail.com>
2020-02-16 19:44:42 -0800

    remove hard coded ticket data

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Ticket from "./Ticket";
-import PropTypes from "prop-types";
 
 function TicketList(){
   return (
-    <Ticket
-    location ='3A'
-    names='thato and haley'
-    issue='firebase will not record'/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </div>
   );
 }
 
